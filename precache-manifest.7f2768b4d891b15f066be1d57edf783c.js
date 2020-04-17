132006726caf3b80450cbd25d6337139783e45b2
mareesdroid <mareesdroid@gmail.com>
2020-04-17 16:02:44 +0530

    Updates

---
+++ precache-manifest.7f2768b4d891b15f066be1d57edf783c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16dbd1a78c31e35b7bbc7ae6ce54499f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "183ab9fd681c03e1be40",
+    "url": "/static/css/2.658eb2da.chunk.css"
+  },
+  {
+    "revision": "600d1cbaebffa9bd21e5",
+    "url": "/static/css/main.4762dd43.chunk.css"
+  },
+  {
+    "revision": "183ab9fd681c03e1be40",
+    "url": "/static/js/2.abc61266.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.abc61266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600d1cbaebffa9bd21e5",
+    "url": "/static/js/main.fdb47343.chunk.js"
+  },
+  {
+    "revision": "2215dd91a20d2d346a74",
+    "url": "/static/js/runtime-main.24fe6747.js"
+  }
+]);
